a10cd4d96c285dc711cdd03f8e516f54761fe380
HuyPhong-21012889-PKA <21012889@st.phenikaa-uni.edu.vn>
2025-10-13 09:03:27 +0700

    seed

--- laravel-php/inventoryManagement/database/seeders/ProductSeeder.php
+++ laravel-php/inventoryManagement/database/seeders/ProductSeeder.php
@@ -9,9 +9,25 @@ class ProductSeeder extends Seeder
 {
     public function run(): void
     {
-        Product::create(['name' => 'Sữa hộp Vinamilk']);
-        Product::create(['name' => 'Mì Hảo Hảo']);
-        Product::create(['name' => 'Đường tinh luyện']);
-        Product::create(['name' => 'Gạo ST25']);
+        Product::create([
+            'name' => 'Sữa hộp Vinamilk',
+            'sku' => 'VINAMILK001', 
+            'price' => 12000,
+            'quantity' => 100,
+        ]);
+
+        Product::create([
+            'name' => 'Bánh mì đặc ruột',
+            'sku' => 'BM001',
+            'price' => 10000,
+            'quantity' => 50,
+        ]);
+
+        Product::create([
+            'name' => 'Cây xây dựng',
+            'sku' => 'CXD001',
+            'price' => 15000,
+            'quantity' => 200,
+        ]);
     }
 }
